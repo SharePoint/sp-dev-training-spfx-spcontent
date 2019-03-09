a86e00723899d2f454f755d3923919c885f2dec9
Rob Windsor <rob.windsor@gmail.com>
2019-03-08 20:02:43 -0500

    Update Ex 2 project files

---
+++ Demos/02-spcrud/src/webparts/spFxHttpClientDemo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpFxHttpClientDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpFxHttpClientDemoWebPartStrings' {
+  const strings: ISpFxHttpClientDemoWebPartStrings;
+  export = strings;
+}
