49f32eb4f3cf2198262ac29ccdda8b9a97f95b05
Rob Windsor <rob.windsor@gmail.com>
2019-08-31 18:11:10 -0400

    Update Ex 1 project files

--- Demos/01-spfxhttpclient/src/webparts/spFxHttpClientDemo/components/SpFxHttpClientDemo.tsx
+++ Demos/01-spfxhttpclient/src/webparts/spFxHttpClientDemo/components/SpFxHttpClientDemo.tsx
@@ -9,20 +9,20 @@ import { escape } from '@microsoft/sp-lodash-subset';
 export default class SpFxHttpClientDemo extends React.Component<ISpFxHttpClientDemoProps, {}> {
   public render(): React.ReactElement<ISpFxHttpClientDemoProps> {
     return (
-      <div className={ styles.spFxHttpClientDemo }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <p className={ styles.title }>SharePoint Content!</p>
-              <a href="#" className={ styles.button } onClick={ this.onGetListItemsClicked }>
-                <span className={ styles.label }>Get Counties</span>
+      <div className={styles.spFxHttpClientDemo}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <p className={styles.title}>SharePoint Content!</p>
+              <a href="#" className={styles.button} onClick={this.onGetListItemsClicked}>
+                <span className={styles.label}>Get Counties</span>
               </a>
             </div>
           </div>
 
-          <div className={ styles.row }>
-            <ul className={ styles.list }>
-              { this.props.spListItems &&
+          <div className={styles.row}>
+            <ul className={styles.list}>
+              {this.props.spListItems &&
                 this.props.spListItems.map((list) =>
                   <li key={list.Id} className={styles.item}>
                     <strong>Id:</strong> {list.Id}, <strong>Title:</strong> {list.Title}
